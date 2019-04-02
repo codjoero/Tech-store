5d59a0eaf132ab91d338cc7110ac4788add106c6
codjoero <rownee2001@gmail.com>
2019-04-02 21:26:42 +0300

    ft-react-router
    - add react router v4
    - setup app routing

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React, { Component}from 'react';
+
+
+export default class Navbar extends Component {
+
+    render() {
+        return (
+            <React.Fragment>
+                <h3>Hello from Navbar</h3>
+            </React.Fragment>
+        );
+    }
+}
